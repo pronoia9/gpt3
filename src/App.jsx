2b24e518f5e7cdd676c2810f0b10794efe611132
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-27 22:14:24 +0300

    import containers

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.scss';
 import { Article, Brand, CTA, Feature, Navbar } from './components';
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
 
 export default function App() {
   return <div className='App'>GPT-3</div>;
