c901ae54a91afbc7941d9dd55624251501d4a4b7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-28 18:54:35 +0300

    map images array

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,6 +1,14 @@
 import './Brand.scss';
-import { atlassian, dropbox, google, shopify, slack } from './imports';
+import { images } from './imports';
 
-export default function Brand () {
-  return <div>Brand</div>;
-};
+export default function Brand() {
+  return (
+    <div className='gpt3__brand section__padding'>
+      {images.map((brand, i) => (
+        <div key={brand}>
+          <img src={brand} alt={`brand-${i}`} />
+        </div>
+      ))}
+    </div>
+  );
+}
