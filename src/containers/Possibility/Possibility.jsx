d8ea10465b86cee40b204c050c8eb97402f37db8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-28 22:41:38 +0300

    finish possibility component

--- src/containers/Possibility/Possibility.jsx
+++ src/containers/Possibility/Possibility.jsx
@@ -7,6 +7,13 @@ export default function Possibility({ data }) {
       <div className='gpt3__possibility-image'>
         <img src={image} alt='possibility' />
       </div>
+
+      <div className='gpt3__possibility-content'>
+        <h4>{pre}</h4>
+        <h1 className='gradient__text'>{title}</h1>
+        <p>{text}</p>
+        <h4>{post}</h4>
+      </div>
     </div>
   );
 }
