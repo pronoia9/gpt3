6016e459f48c34c36a7c607669d843eec188cd76
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-28 19:58:57 +0300

    finish initial feature component

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,5 +1,14 @@
 import './Feature.scss';
 
-export default function Feature() {
-  return <div>Feature</div>;
-};
+export default function Feature({ title = 'NO TITLE', text = 'NO TEXT' }) {
+  return (
+    <div className='gpt3__features-container_feature'>
+      <div className='gpt3__features-container_feature-title'>
+        <div />
+        <h1>{title}</h1>
+      </div>
+
+      <div className='gpt3__features-container_feature-text'>{text}</div>
+    </div>
+  );
+}
