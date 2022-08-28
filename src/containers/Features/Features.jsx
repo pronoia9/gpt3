6ffa726a872aa1f599c991370fa5edf8feee229a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-28 22:02:31 +0300

    add text and map features

--- src/containers/Features/Features.jsx
+++ src/containers/Features/Features.jsx
@@ -1,12 +1,19 @@
+import { Feature } from '../../components';
 import './Features.scss';
 
 export default function Features({ data }) {
   const { title, text, features } = data;
-  
+
   return (
     <div id='features' className='gpt3__features section__padding'>
       <div className='gpt3__features-heading'>
         <h1 className='gradient__text'>{title}</h1>
+        <p>{text}</p>
+      </div>
+      <div className='gpt3__features-container'>
+        {features.map((f) => (
+          <Feature key={f.title} title={f.title} text={f.text} />
+        ))}
       </div>
     </div>
   );
