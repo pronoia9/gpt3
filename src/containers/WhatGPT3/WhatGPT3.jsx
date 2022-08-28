a7dd653527a196f6073bd4e2e7a45b37d4982b7e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-28 19:27:30 +0300

    finish initial component

--- src/containers/WhatGPT3/WhatGPT3.jsx
+++ src/containers/WhatGPT3/WhatGPT3.jsx
@@ -1,5 +1,21 @@
+import { Feature } from '../../components';
 import './WhatGPT3.scss';
 
 export default function WhatGPT3() {
-  return <div>WhatGPT3</div>;
+  return (
+    <div id='whpt3' className='gpt3__whatgpt3 section__margin'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature />
+      </div>
+
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+
+      <div className='gpt3__whatgpt3-container'>
+        <Feature />
+      </div>
+    </div>
+  );
 }
