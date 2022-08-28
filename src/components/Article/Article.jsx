747ad9cfee6f025e0d571111af3beab362dc1072
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-29 00:58:04 +0300

    finish article component

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,5 +1,19 @@
 import './Article.scss';
 
-export default function Article (){
-  return <div>Article</div>;
-};
+export default function Article({ id, title, image, date, button, group }) {
+  return (
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-image'>
+        <img src={image} alt={`blog-${id}`} />
+      </div>
+
+      <div className='gpt3__blog-container_article-content'>
+        <div>
+          <p>{date.toDateString().slice(3).replace(' 202', ', 202')}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>{button.title}</p>
+      </div>
+    </div>
+  );
+}
