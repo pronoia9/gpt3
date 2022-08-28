571c6ba7edd20a7eab4a0ca19f5aeef42685fd3b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-28 20:26:48 +0300

    fix class names

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -2,13 +2,13 @@ import './Feature.scss';
 
 export default function Feature({ title = 'NO TITLE', text = 'NO TEXT' }) {
   return (
-    <div className='gpt3__features-container_feature'>
-      <div className='gpt3__features-container_feature-title'>
+    <div className='gpt3__features-container__feature'>
+      <div className='gpt3__features-container__feature-title'>
         <div />
         <h1>{title}</h1>
       </div>
 
-      <div className='gpt3__features-container_feature-text'>{text}</div>
+      <div className='gpt3__features-container__feature-text'>{text}</div>
     </div>
   );
 }
