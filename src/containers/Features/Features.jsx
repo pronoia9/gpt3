6be534a5a22c20b52e2d63cf19b8002ff6083514
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-28 21:23:33 +0300

    add gradient text + import data

--- src/containers/Features/Features.jsx
+++ src/containers/Features/Features.jsx
@@ -1,5 +1,13 @@
 import './Features.scss';
 
-export default function Features() {
-  return <div>Features</div>;
+export default function Features({ data }) {
+  const { title, text, features } = data;
+  
+  return (
+    <div id='features' className='gpt3__features section__padding'>
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'>{title}</h1>
+      </div>
+    </div>
+  );
 }
