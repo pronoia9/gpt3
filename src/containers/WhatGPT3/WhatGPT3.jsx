23087116fee5fd50d847177bf63ddf5e51244e89
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-28 20:03:05 +0300

    add features object array and map it

--- src/containers/WhatGPT3/WhatGPT3.jsx
+++ src/containers/WhatGPT3/WhatGPT3.jsx
@@ -1,11 +1,27 @@
 import { Feature } from '../../components';
 import './WhatGPT3.scss';
 
+const features = [
+  {
+    title: 'What is GPT-3',
+    text: 'We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.',
+  },
+  { title: 'Chatbots', text: 'We so opinion friends me message as delight. Whole front do of plate heard oh ought.' },
+  {
+    title: 'Knowledgebase',
+    text: 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b',
+  },
+  {
+    title: 'Education',
+    text: 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b',
+  },
+];
+
 export default function WhatGPT3() {
   return (
     <div id='whpt3' className='gpt3__whatgpt3 section__margin'>
       <div className='gpt3__whatgpt3-feature'>
-        <Feature />
+        <Feature title={features[0].title} text={features[0].text} />
       </div>
 
       <div className='gpt3__whatgpt3-heading'>
@@ -14,7 +30,9 @@ export default function WhatGPT3() {
       </div>
 
       <div className='gpt3__whatgpt3-container'>
-        <Feature />
+        {features.slice(1).map((feature) => (
+          <Feature key={feature.title} title={feature.title} text={feature.text} />
+        ))}
       </div>
     </div>
   );
