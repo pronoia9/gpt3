a647e933a026e526a355c4694b8d09e422454cd2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-28 21:23:47 +0300

    add whatGPT3 and features data

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const whatGPT3 = {
+  feature: {
+    title: 'What is GPT-3',
+    text: 'We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.',
+  },
+  title: 'The possibilities are beyond your imagination',
+  text: 'Explore The Library',
+  features: [
+    { title: 'Chatbots', text: 'We so opinion friends me message as delight. Whole front do of plate heard oh ought.' },
+    {
+      title: 'Knowledgebase',
+      text: 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments by.',
+    },
+    {
+      title: 'Education',
+      text: 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments by.',
+    },
+  ],
+};
+
+const features = {
+  title: 'The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.',
+  text: 'Request Early Access to Get Started',
+  features: [
+    {
+      title: 'Improving end distrusts instantly ',
+      text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded. ',
+    },
+    {
+      title: 'Become the tended active',
+      text: 'Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.',
+    },
+    {
+      title: 'Message or am nothing',
+      text: 'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.',
+    },
+    {
+      title: 'Really boy law county',
+      text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.',
+    },
+  ],
+};
+
+export { whatGPT3, features };
