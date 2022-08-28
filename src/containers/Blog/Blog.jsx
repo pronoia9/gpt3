ed594ed2d688a2cbd70b4371fa6d7faf57c6f02d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-29 00:24:36 +0300

    add initial blog

--- src/containers/Blog/Blog.jsx
+++ src/containers/Blog/Blog.jsx
@@ -1,5 +1,17 @@
 import './Blog.scss';
 
-export default function Blog() {
-  return <div>Blog</div>;
+export default function Blog({ data }) {
+  const { title, posts } = data;
+  return (
+    <div id='blog' className='gpt3__blog section__padding'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>{title}</h1>
+      </div>
+
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'></div>
+        <div className='gpt3__blog-container_groupB'></div>
+      </div>
+    </div>
+  );
 }
