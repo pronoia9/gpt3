b3f6267ae32c8b4d9577681e49df7028268aecd1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-28 22:38:30 +0300

    import data, add image

--- src/containers/Possibility/Possibility.jsx
+++ src/containers/Possibility/Possibility.jsx
@@ -1,5 +1,12 @@
 import './Possibility.scss';
 
-export default function Possibility() {
-  return <div>Possibility</div>;
+export default function Possibility({ data }) {
+  const { image, pre, title, text, post } = data;
+  return (
+    <div id='possibility' className='gpt3__possibility section__padding'>
+      <div className='gpt3__possibility-image'>
+        <img src={image} alt='possibility' />
+      </div>
+    </div>
+  );
 }
